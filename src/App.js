0c445e150dd05c80cb0e36894e556e72a73a215a
Danhuri <danhuri.j@gmail.com>
2021-11-19 16:17:06 -0300

    add a notification component to provide user feedback on cart data storage and possible errors

--- src/App.js
+++ src/App.js
@@ -1,39 +1,72 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 import { doc, setDoc } from "firebase/firestore/lite";
-import { db } from './services/firebase-config';
+import { db } from "./services/firebase-config";
+import { uiActions } from "./components/store/ui-slice";
+import Notification from './components/UI/Notifications';
 
 let initialLoad = true;
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector(state => state.cart);
+    const dispatch = useDispatch();
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    const notification = useSelector(state => state.ui.notification);
 
-  useEffect(() => {
-    if (initialLoad) {
-      initialLoad = false;
-      return;
-    }
-    const sendCartToFireStore = async () => {
-      await setDoc(doc(db, "cart", "cart1"), {
-        items: cart.items,
-        totalQuantity: cart.totalQuantity,
-      });
-    };
-    
-    sendCartToFireStore();
+    useEffect(() => {
+        if (initialLoad) {
+            initialLoad = false;
+            return;
+        }
 
-  }, [cart]);
+        const sendCartToFireStore = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "pending",
+                    title: "Sending",
+                    message: "Sending cart data",
+                })
+            );
+            try {
+                await setDoc(doc(db, "cart", "cart1"), {
+                    items: cart.items,
+                    totalQuantity: cart.totalQuantity,
+                });
+            } catch (err) {
+                throw new Error("Sending cart data failed!");
+            }
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Cart data stored successfully",
+                })
+            );
+        };
 
-  return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+        sendCartToFireStore().catch((error) => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "error",
+                    title: "Error!",
+                    message: "Sending cart data failed!",
+                })
+            );
+        });
+    }, [cart, dispatch]);
+
+    return (
+      <Fragment>
+        {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+        <Layout>
+            {showCart && <Cart />}
+            <Products />
+        </Layout>
+      </Fragment>
+    );
 }
 
 export default App;
